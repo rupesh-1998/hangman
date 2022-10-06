6d11450017cc077d56a591a2629ff90faa766f7b
Rupesh Shrestha <rupeshshrestha@Rupeshs-MacBook-Air.local>
2022-10-06 20:56:31 +0100

    final changes and added visuals

--- milestone_3.py
+++ milestone_3.py
@@ -1,8 +1,9 @@
 import random
+from hangman_visuals import lives_visual_dict, win, lose
 
 class Hangman:
     def __init__(self, word_list, num_lives = 5):
-        self.word = random.choice(word_list)
+        self.word = 'aapple'#random.choice(word_list)
         self.word_guessed = ['' for i in self.word]
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
@@ -13,30 +14,49 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f'Good guess! {guess} is in the word.')
-            for i in self.word:
-                if i==guess:
-                    ind = self.word.index(i)
-                    self.word_guessed[ind] = guess
+            inds = [i for i in range(len(self.word)) if self.word[i] == guess]
+            for i in inds:
+                self.word_guessed[i] = guess
             self.num_letters += -1
         else:
-            print(f'Sorry, {guess} is not in the word.')
             self.num_lives += -1
-            print(f'You have {self.num_lives} lives left')
-        #self.list_of_guesses.append(guess)
+            print(f'Sorry, {guess} is not in the word. You have {self.num_lives} lives left.')
+            
+        self.list_of_guesses.append(guess)
+        print(f'Guesses: {self.list_of_guesses}.\n')
 
     def ask_for_input(self):
-        while(True):
-            guess = input('Enter a letter: ')
+        #while(True):
+            print('\n------------------------------------------------------')
+            guess = input('--Enter a letter: ')
             if len(guess)!=1 or guess.isalpha()==False:
                 print('Invalid letter. Please, enter a single alphabetical character.')
             elif guess in self.list_of_guesses:
                 print('You already tried that letter')
             else:
                 self.check_guess(guess)
-                self.list_of_guesses.append(guess)
+                #self.list_of_guesses.append(guess)
 
 
 
+def play_game():
+    word_list = ['apple', 'banana', 'watermelon', 'orange', 'grapefruit']
+    game = Hangman(word_list, num_lives = 7)
 
-word_list = ['apple', 'banana', 'watermelon', 'orange', 'grapefruit']
-Hangman(word_list).ask_for_input()
+    while(True):
+        print(lives_visual_dict[game.num_lives])
+        print('Word:', game.word_guessed)
+        if game.num_lives == 0:
+            print('\nYou lost!')
+            print(lose)
+            print(f'The word was {game.word}.')
+            break
+        if game.num_letters > 0:
+            game.ask_for_input()
+        if game.num_lives != 0 and game.num_letters <= 0:
+            print('\nCongratulations! You won!')
+            print(win)
+            print(f'The word was {game.word}.')
+            break
+
+play_game()
